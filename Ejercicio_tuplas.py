af31b2168cd429b79b759a02196f66c48517503d
GiselleVelasquez29 <gisellevelasquez29@gmail.com>
2026-05-14 12:28:16 -0500

    tuplas ejercicio

--- Ejercicio_tuplas.py
+++ Ejercicio_tuplas.py
@@ -3,10 +3,10 @@
 vector_1 = (1, 2, 3)
 vector_2 = (-1, 0, 2)
 
-producto_escalar = vector_1[0] * vector_2[0] + vector_1[1] * vector_2[1] + vector_1[2] * vector_2[2]
+producto_escalar = vector_1 + vector_2
 print(f"El producto escalar de los vectores es: {producto_escalar}")
 
 #Escribir en un programa que almacene una lista los siguientes precios: 50, 75, 46, 22, 80, 65, 8 y muestre por pantalla el menor y el mayor de los precios.
-precios = [50, 75, 46, 22, 80, 65, 8]
+precios = (50, 75, 46, 22, 80, 65, 8)
 print(f"El precio menor es: {min(precios)}")
 print(f"El precio mayor es: {max(precios)}")
